da7e62b0d9372c29f3b490d826bcee5f7b0d6b5d
Ayyas <shineinouzen429@gmail.com>
2025-10-01 15:22:47 +0700

    Tugas PPDB

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Homepage from "./Pages/Homepage";
+
+function App() {
+   return (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Homepage/>}/>
+    </Routes>
+  </BrowserRouter>
+  )
+}
+
+export default App
---
+++ src/Component/Menu1.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Menu1() {
+  return (
+    <div>Menu1</div>
+  )
+}
+
+export default Menu1
