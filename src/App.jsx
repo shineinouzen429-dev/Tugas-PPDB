175c3d0ae4f0b4090f3e6c120d71414cb2c773a5
Ayyas <shineinouzen429@gmail.com>
2025-10-02 15:01:39 +0700

    Tugas PPDB

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Homepage from "./Pages/Homepage";
+import Login from "./Pages/Login";
 
 function App() {
    return (
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<Homepage/>}/>
+      <Route path="/L" element={<Login/>}/>
     </Routes>
   </BrowserRouter>
   )
--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -2,6 +2,26 @@ import React from 'react'
 
 export const Login = () => {
   return (
-    <div>Login</div>
+    <div className='bg-orange-500 h-screen items-center justify-center flex'>
+      <div className='bg-black p-8 rounded-lg shadow-md w-full max-w-sm justify-center items-center'>
+        <p className='text-white'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque hic molestiae
+           consequuntur eum a accusantium ipsum amet dolores voluptas dolore. Repellendus
+            impedit ducimus molestias consequuntur, dolorem quas non obcaecati, deserunt
+             sapiente magni unde aspernatur praesentium accusamus accusantium amet tempora.
+              Inventore illo atque, ipsam corrupti laudantium itaque delectus, vel sapiente,
+               fugiat repudiandae dignissimos dolorem necessitatibus mollitia temporibus
+                numquam possimus soluta non tenetur qui voluptas. Totam veniam eveniet impedit
+                 quisquam minus debitis?
+        </p>
+          <button className="px-30 py-4 bg-orange-400 ml-5 mt-5 font-semibold rounded-xl shadow-lg">Pintar?
+          </button>
+          <button className="px-6 py-3 text-white ml-25 font-semibold rounded-xl shadow-lg transition">
+            belum pintar?
+          </button>
+
+      </div>
+    </div>
   )
 }
+
+export default Login
